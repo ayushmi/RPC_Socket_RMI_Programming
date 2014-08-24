bd5ce3651ae7e659d5edb1e7f4c516036c2f48c1
ayush mittal <ayushsmittal@gmail.com>
2014-08-24 12:19:28 +0530

    NextPage.java now working

--- RMI/NextPage.java
+++ RMI/NextPage.java
@@ -1,29 +1,32 @@
 import javax.swing.*;
+import javax.swing.*;
+import java.sql.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.rmi.*;
 
 class NextPage extends JFrame
 {
-  
+  RoomInterface  Rooms; 
   NextPage(String st)
-   {
+  {
     try{
-    Rooms = (RoomInterface)Naming.lookup("//localhost/Rooms");
+      Rooms = (RoomInterface)Naming.lookup("//localhost/Rooms");
+    }
+    catch (Exception e) {
+      System.out.println("Roomclient exception: " + e);
+    }
+    setLayout(null);
+
+    setFocusable(true);// INSERT THIS
+    setBackground(Color.green);
+    setDefaultCloseOperation(javax.swing. WindowConstants.DISPOSE_ON_CLOSE);
+    setTitle("Welcome");
+    JLabel lab=new JLabel("Welcome  "+st);
+
+    lab.setBounds(10,10,500,20);
+    add(lab);
+
+    setSize(1024, 768);
   }
-  catch (Exception e) {
-          System.out.println("Roomclient exception: " + e);
-  }
-      setLayout(null);
-
-  setFocusable(true);// INSERT THIS
-  setBackground(Color.green);
-     setDefaultCloseOperation(javax.swing. WindowConstants.DISPOSE_ON_CLOSE);
-     setTitle("Welcome");
-     JLabel lab=new JLabel("Welcome  "+st);
-
-     lab.setBounds(10,10,500,20);
-     add(lab);
-
-       setSize(1024, 768);
-      }
  }
